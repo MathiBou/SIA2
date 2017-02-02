baa98619b9842d693abb34a9cce0a7b4cc7b55ed
LaurianeC <lauriane.chilaud@hotmail.fr>
2017-02-02 12:10:50 +0100

    Update skeletonIO.h

--- characterAnimNonDep/include/skeletonIO.h
+++ characterAnimNonDep/include/skeletonIO.h
@@ -4,7 +4,7 @@
 #include "skeleton.h"
 
 Skeleton* readHierarchy(std::ifstream &inputfile);
-Skeleton* interpolate(std::ifstream &inputfile1, std::ifstream &inputfile2 );
+Skeleton* interpolate(std::ifstream &inputfile1, std::ifstream &inputfile2);
 Skeleton* transition(std::ifstream &inputfile1, std::ifstream &inputfile2);
 
 void readJoint(std::ifstream &inputfile, Skeleton* parent);
@@ -15,7 +15,7 @@ void interpolateMotion(std::ifstream &inputfile1, std::ifstream &inputfile2, Ske
 std::vector<double> transitionMotion(std::ifstream &inputfile1, Skeleton* root, double lastX, double lastY, double lastZ);
 
 
-void readKeyFrame(std::ifstream &inputfile, Skeleton* skel, double Xinit = 0, double Yinit =0, double Zinit = 0);
+void readKeyFrame(std::ifstream &inputfile, Skeleton* skel, bool isFirstFrame, double Xinit = 0, double Yinit = 0, double Zinit = 0);
 void interpolateKeyFrame(std::ifstream &inputfile1, std::ifstream &inputfile2, Skeleton* skel);
 
 void defineRotateOrder(Skeleton *skel);
